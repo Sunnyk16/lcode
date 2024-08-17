ec9a56855bad5b2c8c5363437a0ec0f894dcd78a
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-17 16:56:46 +0530

    first commit

---
+++ palindroneNumber.java
@@ -0,0 +1,30 @@
+class palindroneNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int n = x;
+        int revNum = 0;
+        while (n > 0) {
+            int d = n % 10;
+
+            revNum = revNum * 10 + d;
+
+            n = n / 10;
+        }
+
+        if(revNum == x){
+            return true;
+        }
+        else{
+            return false;
+        }   
+
+    }
+    public static void main(String[] args) {
+        palindroneNumber obj = new palindroneNumber();
+        System.out.println(obj.isPalindrome(121));
+    }
+}
