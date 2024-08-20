336aaa7d9d60d9f1224a625c92c7f7c684eef946
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-20 23:44:38 +0530

    added code

---
+++ Add-to-Array-Form/AddToArrayForm.java
@@ -0,0 +1,9 @@
+// package Add-to-Array-Form;
+
+public class AddToArrayForm {
+    public static void main(String[] args){
+        System.out.println(4/2);
+        System.out.println(4%2);
+    }
+    
+}
