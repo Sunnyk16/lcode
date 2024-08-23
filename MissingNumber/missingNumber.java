9beea6466dae7d4e1eb6c6cd43d4093342025a5e
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-23 17:07:15 +0530

    feat: Add missingNumber class to calculate missing number in an array

---
+++ MissingNumber/missingNumber.java
@@ -0,0 +1,20 @@
+// package MissingNumber;
+
+public class missingNumber {
+    public int missingNumber(int[] nums){
+
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        int sumNums = 0;
+        for(int num : nums){
+            sumNums += num;
+        }
+        return sum - sumNums;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        missingNumber mn = new missingNumber();
+        System.out.println(mn.missingNumber(nums));
+    }
+}
