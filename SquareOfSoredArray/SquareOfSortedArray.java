7620d1fb9384299f7f4bde093a93097e66ba3c76
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-24 09:57:27 +0530

    feat: Implement SquareOfSortedArray class to calculate the square of a sorted array

---
+++ SquareOfSoredArray/SquareOfSortedArray.java
@@ -0,0 +1,35 @@
+// package SquareOfSoredArray;
+
+public class SquareOfSortedArray {
+
+    public int[] squareArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        int start = 0;
+        int end = nums.length - 1;
+        int ptr = ans.length - 1;
+
+        while (start <= end) {
+            int ss = nums[start] * nums[start];
+            int es = nums[end] * nums[end];
+            if (ss > es) {
+                ans[ptr] = ss;
+                start++;
+            } else {
+                ans[ptr] = es;
+                end--;
+            }
+
+            ptr--;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -4, -1, 0, 3, 10 };
+        SquareOfSortedArray sosa = new SquareOfSortedArray();
+        int[] ans = sosa.squareArray(nums);
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+}
