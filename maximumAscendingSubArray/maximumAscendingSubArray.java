102e42b9ccced563aab2b1108c636978f7c16a49
Sunnyk16 <kumarsunny1246@gmail.com>
2024-08-31 23:31:31 +0530

    feat: Optimize maximumAscendingSubArray class for finding the maximum ascending subarray

---
+++ maximumAscendingSubArray/maximumAscendingSubArray.java
@@ -0,0 +1,25 @@
+// package maximumAscendingSubArray;
+
+public class maximumAscendingSubArray {
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int currmax = nums[0];
+
+        for(int i=1 ; i<nums.length;i++){
+            if(nums[i-1]<nums[i]){
+                currmax = currmax + nums[i];
+            }else {
+                max = Math.max(max , currmax);
+                currmax = nums[i];
+            }
+        }
+         max = Math.max(max , currmax);
+         return max ;
+    }
+    public static void main(String[] args) {
+        maximumAscendingSubArray obj = new maximumAscendingSubArray();
+        int[] nums = {10,20,30,5,10,50};
+        System.out.println(obj.maxAscendingSum(nums));
+    }
+    
+}
