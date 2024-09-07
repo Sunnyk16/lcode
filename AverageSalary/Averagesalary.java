3bb98c489c36599560bcb77a20dc1bc0c485b004
Sunnyk16 <kumarsunny1246@gmail.com>
2024-09-07 23:37:28 +0530

    Optimize Averagesalary class for calculating the average salary of an array

---
+++ AverageSalary/Averagesalary.java
@@ -0,0 +1,35 @@
+// package AverageSalary;
+
+public class Averagesalary {
+    
+        public double average(int[] salary) {
+            int max = salary[0];
+            int min = salary[0];
+            int sum = salary[0];
+    
+            for(int i=1;i<salary.length;i++){
+    
+                if(max<salary[i]){
+                    max=salary[i];
+                }
+    
+                if(min>salary[i]){
+                    min=salary[i];
+                }
+    
+                sum = sum + salary[i];
+            }
+    
+            sum = sum - min - max ;
+    
+            double ans = (double)sum/(salary.length-2);
+            return ans ;
+        
+    }
+    public static void main(String[] args) {
+        Averagesalary obj = new Averagesalary();
+        int[] salary = { 4000,3000,1000,2000 };
+        System.out.println(obj.average(salary));
+    }
+    
+}
