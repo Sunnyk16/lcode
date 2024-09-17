cdc5aed42e060560676a1fc88e98483e6829d440
Sunnyk16 <kumarsunny1246@gmail.com>
2024-09-17 21:54:37 +0530

    Optimize BoatstoSavePeople class for finding the minimum number of boats required to save people

---
+++ Boats-to-Save-People/BoatstoSavePeople.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+
+// package Boats-to-Save-People;
+
+public class BoatstoSavePeople {
+    public int numRescueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length-1;
+        int count=0;
+
+        while(start<=end){
+            if(people[start]+people[end]<=limit){
+                start++;
+                end--;
+            }
+            else{
+                end--;
+            }
+            count++;
+        }
+        return count;
+        
+    }
+    public static void main(String[] args){
+        BoatstoSavePeople bsp = new BoatstoSavePeople();
+        int[] people = {3,2,2,1};
+        int limit = 3;
+        System.out.println(bsp.numRescueBoats(people, limit));
+    }
+    
+}
