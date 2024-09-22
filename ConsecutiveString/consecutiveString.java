6474a7c8a07f7009410c48e515a1346548038695
Sunnyk16 <kumarsunny1246@gmail.com>
2024-09-22 23:26:34 +0530

    Optimize consecutiveString class for finding the maximum consecutive power in a string

---
+++ ConsecutiveString/consecutiveString.java
@@ -0,0 +1,29 @@
+// package ConsecutiveString;
+
+public class consecutiveString {
+    public int maxPower(String s) {
+        int max= 1;
+        int count =1;
+         for(int i =1;i< s.length();i++){
+            char curr=s.charAt(i);
+            char prev=s.charAt(i-1);
+
+            if(curr == prev){
+                count++;
+            }
+            else{
+                max=Math.max(max,count);
+                count=1;
+            }
+         }
+         max=Math.max(max,count);
+         return max;
+        
+    }
+    public static void main(String[] args) {
+        consecutiveString cs = new consecutiveString();
+        String s = "leetcode";
+        System.out.println(cs.maxPower(s));
+    }
+    
+}
