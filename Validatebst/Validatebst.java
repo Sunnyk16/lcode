aeea4adb7293205f23adec30542bb6dd31977979
Sunnyk16 <kumarsunny1246@gmail.com>
2024-11-04 16:29:42 +0530

    Add Validatebst class for validating a binary search tree

---
+++ Validatebst/Validatebst.java
@@ -0,0 +1,49 @@
+// package Validatebst;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){this.val=val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val=val;
+        this.left=left;
+        this.right=right;
+    }
+}
+
+public class Validatebst {
+
+    public boolean isValidBST(TreeNode root) {
+        
+        return helper( root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root,long min,long max){
+        if(root==null){
+            return true;
+
+        }
+        if(root.val<=min || root.val>=max){
+            return false;
+        }
+
+        boolean leftans=helper(root.left,min,root.val);
+        boolean rightans=helper(root.right,root.val,max);
+
+
+        return leftans && rightans;
+    }
+    public static void main(String[] args) {
+        Validatebst obj = new Validatebst();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        boolean result = obj.isValidBST(root);
+        System.out.println(result);
+    }
+    
+}
