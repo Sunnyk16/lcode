e14a02a34b1ac2e1fbc98eb5478b50ce4e9452b0
Sunnyk16 <kumarsunny1246@gmail.com>
2024-11-12 23:32:40 +0530

    Add maxAscendingSum class for calculating the maximum ascending sum in an array

---
+++ maxAscendingSum/maxAscendingSum.java
@@ -0,0 +1,29 @@
+// package maxAscendingSum;
+
+public class maxAscendingSum {
+    class Solution {
+        public int maxAscendingSum(int[] nums) {
+            int max = nums[0];
+            int currmax = nums[0];
+    
+            for(int i=1 ; i<nums.length;i++){
+                if(nums[i-1]<nums[i]){
+                    currmax = currmax + nums[i];
+                }else {
+                    max = Math.max(max , currmax);
+                    currmax = nums[i];
+                }
+            }
+             max = Math.max(max , currmax);
+             return max ;
+        }
+    }
+    public static void main(String[] args) {
+        // Example usage of maxAscendingSum method
+        int[] nums = {10,20,30,5,10,50};
+        Solution sol = new Solution();
+        int ans = sol.maxAscendingSum(nums);
+        System.out.println(ans);
+    }
+    
+}
