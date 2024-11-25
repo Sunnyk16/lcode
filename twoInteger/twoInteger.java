9a9ce1dc42fcf1663162287b3eb98956cf11c50b
Sunnyk16 <kumarsunny1246@gmail.com>
2024-11-25 22:12:11 +0530

    Add twoInteger class with method to perform integer division

---
+++ twoInteger/twoInteger.java
@@ -0,0 +1,36 @@
+package twoInteger;
+
+public class twoInteger {
+    
+        public int divide(int a, int b) {
+            if (b == 1) {
+                return a;
+            }
+            if (a == Integer.MIN_VALUE && b == -1) {
+                return Integer.MAX_VALUE;
+            }
+            boolean sign = (a > 0 && b > 0) || (a < 0 && b < 0);
+            a = a > 0 ? -a : a;
+            b = b > 0 ? -b : b;
+            int ans = 0;
+            while (a <= b) {
+                int x = b;
+                int cnt = 1;
+                while (x >= (Integer.MIN_VALUE >> 1) && a <= (x << 1)) {
+                    x <<= 1;
+                    cnt <<= 1;
+                }
+                ans += cnt;
+                a -= x;
+            }
+            return sign ? ans : -ans;
+        }
+        public static void main(String[] args) {    
+            twoInteger ti = new twoInteger();
+            int a = 10;
+            int b = 3;
+            System.out.println(ti.divide(a, b));
+        }
+    
+
+}
