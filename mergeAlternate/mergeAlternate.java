59468224b3a8f9ac8c7c5670d318d46e5ee72af5
Sunnyk16 <kumarsunny1246@gmail.com>
2024-12-15 23:13:40 +0530

    Add mergeAlternate class with method to merge two strings alternately

---
+++ mergeAlternate/mergeAlternate.java
@@ -0,0 +1,26 @@
+
+
+public class mergeAlternate {
+    
+
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < m || i < n; ++i) {
+            if (i < m) {
+                ans.append(word1.charAt(i));
+            }
+            if (i < n) {
+                ans.append(word2.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String word1 = "abc", word2 = "pqr";
+        mergeAlternate ma = new mergeAlternate();
+        System.out.println(ma.mergeAlternately(word1, word2));
+    }
+    
+}
