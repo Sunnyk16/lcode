d41ecdfb9b1c98312f6cce204bcf5caba3c82f55
Sunnyk16 <kumarsunny1246@gmail.com>
2024-12-16 23:27:35 +0530

    Add sudokuSolver class with method to solve Sudoku puzzles using backtracking

---
+++ sudokuSolver/sudokuSolver.java
@@ -0,0 +1,72 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sudokuSolver {
+    
+    private boolean ok;
+    private char[][] board;
+    private List<Integer> t = new ArrayList<>();
+    private boolean[][] row = new boolean[9][9];
+    private boolean[][] col = new boolean[9][9];
+    private boolean[][][] block = new boolean[3][3][9];
+
+    public void solveSudoku(char[][] board) {
+        this.board = board;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    t.add(i * 9 + j);
+                } else {
+                    int v = board[i][j] - '1';
+                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
+                }
+            }
+        }
+        dfs(0);
+    }
+
+    private void dfs(int k) {
+        if (k == t.size()) {
+            ok = true;
+            return;
+        }
+        int i = t.get(k) / 9, j = t.get(k) % 9;
+        for (int v = 0; v < 9; ++v) {
+            if (!row[i][v] && !col[j][v] && !block[i / 3][j / 3][v]) {
+                row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
+                board[i][j] = (char) (v + '1');
+                dfs(k + 1);
+                row[i][v] = col[j][v] = block[i / 3][j / 3][v] = false;
+            }
+            if (ok) {
+                return;
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        sudokuSolver s = new sudokuSolver();
+        s.solveSudoku(board);
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
