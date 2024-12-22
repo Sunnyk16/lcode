ae6b9e5fe2707a3083051ed3d0c05235d026992b
Sunnyk16 <kumarsunny1246@gmail.com>
2024-12-22 23:36:07 +0530

    Add majorityElement class with method to find the majority element in an array

---
+++ majaorityElement/majaorityElement.java
@@ -0,0 +1,23 @@
+public class majaorityElement {
+    
+        public int majorityElement(int[] nums) {
+            int cnt = 0, m = 0;
+            for (int x : nums) {
+                if (cnt == 0) {
+                    m = x;
+                    cnt = 1;
+                } else {
+                    cnt += m == x ? 1 : -1;
+                }
+            }
+            return m;
+        }
+
+    public static void main(String[] args) {
+        majaorityElement me = new majaorityElement();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(me.majorityElement(nums));
+    }
+    
+    
+}
