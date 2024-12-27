3ad3af74a83a3b2dc543e8fc16b41ca4fb030333
Sunnyk16 <kumarsunny1246@gmail.com>
2024-12-27 23:08:21 +0530

    Add gcd class with method to compute the greatest common divisor of two strings

---
+++ gcd/gcd.java
@@ -0,0 +1,22 @@
+
+
+public class gcd {
+    
+        public String gcdOfStrings(String str1, String str2) {
+            if (!(str1 + str2).equals(str2 + str1)) {
+                return "";
+            }
+            int len = gcd(str1.length(), str2.length());
+            return str1.substring(0, len);
+        }
+    
+        private int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+        public static void main(String[] args) {
+            gcd g = new gcd();
+            System.out.println(g.gcdOfStrings("ABCABC", "ABC"));
+        }
+    
+    
+}
