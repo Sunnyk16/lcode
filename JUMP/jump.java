36aef633b5490be9e57123b087f6114735fea46d
Sunnyk16 <kumarsunny1246@gmail.com>
2024-12-30 22:14:25 +0530

    Add jump class with method to determine if one can reach the last index

---
+++ JUMP/jump.java
@@ -0,0 +1,23 @@
+package JUMP;
+
+public class jump {
+    
+        public boolean canJump(int[] nums) {
+            int mx = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                if (mx < i) {
+                    return false;
+                }
+                mx = Math.max(mx, i + nums[i]);
+            }
+            return true;
+        }
+        public static void main(String[] args) {
+            jump solution = new jump();
+            int[] nums = {2, 3, 1, 1, 4};
+            boolean result = solution.canJump(nums);
+            System.out.println(result);
+        }
+    
+    
+}
