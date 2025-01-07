4be2404cf9b63e0b27edcf088235893765609dd1
Sunnyk16 <kumarsunny1246@gmail.com>
2025-01-07 22:47:39 +0530

    Add largestAltitude method to calculate the highest altitude from an array of gains

---
+++ HightestAltitude/altitude.java
@@ -0,0 +1,16 @@
+class altiutde{
+    
+        public int largestAltitude(int[] gain) {
+            int ans = 0, h = 0;
+            for (int v : gain) {
+                h += v;
+                ans = Math.max(ans, h);
+            }
+            return ans;
+        }
+        public static void main(String[] args) {
+            int[] gain = {-5,1,5,0,-7};
+            System.out.println(largestAltitude(gain));
+        }
+    
+}
