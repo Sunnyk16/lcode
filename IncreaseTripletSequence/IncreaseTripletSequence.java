929f03cbb05af1ffbee95ce8ea8e7c32f6d4587f
Sunnyk16 <kumarsunny1246@gmail.com>
2025-01-09 20:59:28 +0530

    Refactor increasingTriplet method for clarity and efficiency; restore main method for testing

--- IncreaseTripletSequence/IncreaseTripletSequence.java
+++ IncreaseTripletSequence/IncreaseTripletSequence.java
@@ -2,38 +2,59 @@
 
 public class IncreaseTripletSequence {
     
-    public boolean increasingTriplet(int[] nums) {
+    // public boolean increasingTriplet(int[] nums) {
 
-        int first = Integer.MAX_VALUE;
-        int second = Integer.MAX_VALUE;
-        int third = Integer.MAX_VALUE;
+    //     int first = Integer.MAX_VALUE;
+    //     int second = Integer.MAX_VALUE;
+    //     int third = Integer.MAX_VALUE;
         
 
-        for (int i = 0; i < nums.length; i++) {
+    //     for (int i = 0; i < nums.length; i++) {
 
-            int ele = nums[i];
+    //         int ele = nums[i];
 
-            if (ele <= first) {
-                first = ele;
-            } 
-             else if (ele <= second) {
-                second = ele;
+    //         if (ele <= first) {
+    //             first = ele;
+    //         } 
+    //          else if (ele <= second) {
+    //             second = ele;
 
-            } else {
-                third = ele;
+    //         } else {
+    //             third = ele;
 
-                return true;
+    //             return true;
+    //         }
+
+    //     }
+    //     return false;
+
+    // }
+
+    // public static void main(String[] args) {
+    //     IncreaseTripletSequence obj = new IncreaseTripletSequence();
+    //     int[] nums = {1, 2, 3, 4, 5};
+    //     System.out.println(obj.increasingTriplet(nums));
+    // }
+    
+        public boolean increasingTriplet(int[] nums) {
+            int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
+            for (int num : nums) {
+                if (num > mid) {
+                    return true;
+                }
+                if (num <= min) {
+                    min = num;
+                } else {
+                    mid = num;
+                }
             }
-
+            return false;
         }
-        return false;
-
-    }
-
-    public static void main(String[] args) {
-        IncreaseTripletSequence obj = new IncreaseTripletSequence();
-        int[] nums = {1, 2, 3, 4, 5};
-        System.out.println(obj.increasingTriplet(nums));
-    }
+        public static void main(String[] args) {
+            IncreaseTripletSequence obj = new IncreaseTripletSequence();
+            int[] nums = {1, 2, 3, 4, 5};
+            System.out.println(obj.increasingTriplet(nums));
+        }
+    
     
 }
