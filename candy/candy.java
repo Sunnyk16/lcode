f5637d759e1073dcd5fa0c9fb8af94922ad30312
Sunnyk16 <kumarsunny1246@gmail.com>
2025-01-10 22:05:36 +0530

    Added candy method to distribute candies based on ratings; include main method for testing

---
+++ candy/candy.java
@@ -0,0 +1,35 @@
+public class candy {
+    
+    
+        public int candy(int[] ratings) {
+            int n = ratings.length;
+            int up = 0;
+            int down = 0;
+            int peak = 0;
+            int candies = 1;
+            for (int i = 1; i < n; i++) {
+                if (ratings[i - 1] < ratings[i]) {
+                    up++;
+                    peak = up + 1;
+                    down = 0;
+                    candies += peak;
+                } else if (ratings[i] == ratings[i - 1]) {
+                    peak = 0;
+                    up = 0;
+                    down = 0;
+                    candies++;
+                } else {
+                    down++;
+                    up = 0;
+                    candies += down + (peak > down ? 0 : 1);
+                }
+            }
+            return candies;
+        }
+        public static void main(String[] args) {
+            candy obj = new candy();
+            int[] ratings = {1, 0, 2};
+            System.out.println(obj.candy(ratings));
+        }
+    
+}
