bc056153e39911cb28cd673b84f4770fde2b333b
Sunnyk16 <kumarsunny1246@gmail.com>
2025-02-07 21:40:30 +0530

    Update right child value in Validatebst for testing

--- Validatebst/Validatebst.java
+++ Validatebst/Validatebst.java
@@ -38,7 +38,7 @@ public class Validatebst {
         Validatebst obj = new Validatebst();
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
-        root.right = new TreeNode(7);
+        root.right = new TreeNode(6);
         root.left.left = new TreeNode(1);
         root.left.right = new TreeNode(3);
 
