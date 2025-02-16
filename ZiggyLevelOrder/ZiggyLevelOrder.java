f725910672f78d4d5a38f5e9e7a2420425b840d2
Sunnyk16 <kumarsunny1246@gmail.com>
2025-02-16 21:30:51 +0530

    Update right child value in ZiggyLevelOrder for testing

--- ZiggyLevelOrder/ZiggyLevelOrder.java
+++ ZiggyLevelOrder/ZiggyLevelOrder.java
@@ -61,7 +61,7 @@ public static void main(String[] args) {
         ZiggyLevelOrder.Solution solution = ziggyLevelOrder.new Solution();
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
+        root.right = new TreeNode(19);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
         List<List<Integer>> ans = solution.zigzagLevelOrder(root);
