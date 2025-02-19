c9f7bd4363526f092a8386eeaa39afa62712f3a0
Sunnyk16 <kumarsunny1246@gmail.com>
2025-02-19 21:50:08 +0530

    Update left child values in bstgst for testing

--- bstgst/bstgst.java
+++ bstgst/bstgst.java
@@ -31,10 +31,10 @@ public class bstgst {
     public static void main(String[] args) {
         bstgst obj = new bstgst();
         TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(1);
+        root.left = new TreeNode(2);
         root.right = new TreeNode(6);
         root.left.left = new TreeNode(0);
-        root.left.right = new TreeNode(2);
+        root.left.right = new TreeNode(1);
         root.left.right.right = new TreeNode(3);
         root.right.left = new TreeNode(5);
         root.right.right = new TreeNode(7);
