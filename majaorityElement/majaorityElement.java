7ffca53b1b27d02687b7e87c19e04f1531cd491c
Sunnyk16 <kumarsunny1246@gmail.com>
2025-03-09 16:51:19 +0530

    Refactor majorityElement method for improved readability

--- majaorityElement/majaorityElement.java
+++ majaorityElement/majaorityElement.java
@@ -1,23 +1,24 @@
+
 public class majaorityElement {
-    
-        public int majorityElement(int[] nums) {
-            int cnt = 0, m = 0;
-            for (int x : nums) {
-                if (cnt == 0) {
-                    m = x;
-                    cnt = 1;
-                } else {
-                    cnt += m == x ? 1 : -1;
-                }
+
+    public int majorityElement(int[] nums) {
+        int cnt = 0, m = 0;
+        for (int x : nums) {
+            if (cnt == 0) {
+                m = x;
+                cnt = 1;
+            } else {
+                cnt += m == x ? 1 : -1;
             }
-            return m;
         }
+        return m;
+    }
 
     public static void main(String[] args) {
         majaorityElement me = new majaorityElement();
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
         System.out.println(me.majorityElement(nums));
-        int[] nums2 = {3, 3, 4, 2,2, 4, 4, 2, 4};
+        int[] nums2 = {3, 3, 4, 2, 2, 4, 3,  4, 2, 4};
         System.out.println(me.majorityElement(nums2));
 
     }
