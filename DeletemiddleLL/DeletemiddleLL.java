4c0ced6a4d45148d51ac6cce3d18bc27e2c5c007
Sunnyk16 <kumarsunny1246@gmail.com>
2025-03-20 21:26:34 +0530

    Add additional node with value 6 in DeletemiddleLL example for testing

--- DeletemiddleLL/DeletemiddleLL.java
+++ DeletemiddleLL/DeletemiddleLL.java
@@ -32,6 +32,7 @@ public class DeletemiddleLL {
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
         DeletemiddleLL obj = new DeletemiddleLL();
         ListNode res = obj.deleteMiddle(head);
         while(res!=null){
