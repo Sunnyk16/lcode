b5d4c8cddd8216ef2af485774d72e0ac9fb6071c
Sunnyk16 <kumarsunny1246@gmail.com>
2025-03-22 22:34:41 +0530

    Add additional nodes to CountNodes example for testing

--- countNodes/CountNodes.java
+++ countNodes/CountNodes.java
@@ -29,6 +29,8 @@ public class CountNodes {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
         System.out.println(countNodes.countNodes(root));
     }
     
